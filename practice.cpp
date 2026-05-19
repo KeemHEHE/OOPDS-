aec7f28066ff251a0b165b27630300494d55a5de
KeemHEHE <adamhakeemnjr@gmail.com>
2026-05-19 23:35:41 +0800

    add a new freshbox

--- practice.cpp
+++ practice.cpp
@@ -6,12 +6,16 @@ class Box {
         int number;
 
     public:
+        // constructor - runs automatically when box is created
+        // sets number to 0 by default
+        Box() {
+            number = 0;
+        }
          void setNumber (int n) {
             // only allow numbers between -128 and 127
             if (n>127) {
                 cout << "TOO BIG! Max is 127" <<endl;
                 number = 127; // store max value
-            }
             } else if (n<-128) {
                 cout << "TOO SMALL! Min is -128" <<endl;
                 number = -128; // store min value
@@ -40,6 +44,23 @@ int main() {
     //test too small number
     myBox.setNumber(-200);
     cout << "Value:" << myBox.getNumber() << endl;
+
+    //read before putting anything in
+    cout << "Default value:" << myBox.getNumber() << endl;
+
+    //now put something in
+    myBox.setNumber(42);
+    cout << "After setting:" << myBox.getNumber() << endl;
+
+    //test freshBox
+    Box freshBox; //brand new box didnt store old value auto set = 0
+    cout << "Default value:" << freshBox.getNumber() << endl;
+
+    freshBox.setNumber(42);
+    cout << "After setting:" << freshBox.getNumber() << endl;
+
+
+
 
     return 0;
 }
